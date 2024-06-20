7c681e028603adb72cebb514acf34b4042564207
arieltroian <arieltroian@outlook.com>
2024-06-20 20:55:21 -0300

    feat: add initial project setup configurations

---
+++ src/test/java/com/example/catalogo_de_livros/CatalogoDeLivrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.catalogo_de_livros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogoDeLivrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
