167df7b3a4fd33c2365d911eef1cb031baf71d25
arieltroian <arieltroian@outlook.com>
2024-06-22 18:44:43 -0300

    feat: create BookResponseDTO class for data transfer

---
+++ src/main/java/com/example/catalogo_de_livros/book/BookResponseDTO.java
@@ -0,0 +1,7 @@
+package com.example.catalogo_de_livros.book;
+
+public record BookResponseDTO(Long id, String title, String description, String author , String image, Integer price) {
+    public BookResponseDTO(Book book){
+        this(book.getId(), book.getTitle(), book.getDescription(), book.getAuthor(), book.getImage(), book.getPrice());
+    }
+}
