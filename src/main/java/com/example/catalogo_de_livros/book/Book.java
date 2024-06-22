1326566b3e603ac4ad24ef87bc317b3d3a619246
arieltroian <arieltroian@outlook.com>
2024-06-22 18:38:13 -0300

    feat: create Book class to represent books in the database

---
+++ src/main/java/com/example/catalogo_de_livros/book/Book.java
@@ -0,0 +1,31 @@
+package com.example.catalogo_de_livros.book;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "books")
+@Entity(name = "books")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Book {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private String author;
+    private String image;
+    private Integer price;
+
+    public Book(BookRequestDTO data){
+        this.image = data.image();
+        this.price = data.price();
+        this.title = data.title();
+        this.description = data.description();
+        this.author = data.author();
+    }
+}
